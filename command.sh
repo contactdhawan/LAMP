9d49699706e8ae9b59eca63d5d26870ee1b85130
Kumar <neeraj.o.kumar@capgemini.com>
2020-05-24 23:20:29 -0500

    basic structure

---
+++ command.sh
@@ -0,0 +1,5 @@
+# source of truth
+# https://github.com/aws-samples/serverless-app-examples/blob/master/python/hello-world-python3
+
+# command to create s3 bucket
+aws s3 mb s3://samlambdaneerajbuck

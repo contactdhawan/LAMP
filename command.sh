cdd91b5778d6d65e28f44b24a8f9c21e955e71eb
Kumar <neeraj.o.kumar@capgemini.com>
2020-05-25 00:46:17 -0500

    adding api

--- command.sh
+++ command.sh
@@ -9,3 +9,6 @@ sam package --s3-bucket samlambdaneerajbuck -t template.yaml --output-template-f
 
 # sam deploy
 sam deploy --template-file target\state.yaml --stack-name LAMPSTACK --capabilities CAPABILITY_IAM
+
+# delete
+aws cloudformation delete-stack --stack-name LAMPSTACK

862edbb85774f0c728a386f88a172eba4529f945
Kumar <neeraj.o.kumar@capgemini.com>
2020-05-24 23:25:33 -0500

    package

--- command.sh
+++ command.sh
@@ -3,3 +3,6 @@
 
 # command to create s3 bucket
 aws s3 mb s3://samlambdaneerajbuck
+
+# sam package 
+sam package --s3-bucket samlambdaneerajbuck -t template.yaml --output-template-file target/state.yaml 

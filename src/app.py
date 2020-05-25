95044e005b5d3ee07d6cc50d6143352e91143c40
Kumar <neeraj.o.kumar@capgemini.com>
2020-05-24 23:33:55 -0500

    adding api

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
 import json
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
 def lambda_handler(event, context):
-    return "Hello world"
+    return respond(NONE,"Hello World")
 
